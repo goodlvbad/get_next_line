763017a67be623d59c9ffc3046f8d7638c020e89
Oakenshield Earlene <oearlene@ga-n6.21-school.ru>
2019-10-30 04:01:55 +0300

    norme errors fixed

--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,25 @@
 /*   By: oearlene <oearlene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 05:08:20 by oearlene          #+#    #+#             */
-/*   Updated: 2019/10/30 02:26:12 by oearlene         ###   ########.fr       */
+/*   Updated: 2019/10/30 03:26:54 by oearlene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define BUFF_SIZE 35
+# define BUFF_SIZE 35
 
-#include <unistd.h>
-#include <fcntl.h>
-#include "libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft/libft.h"
 
-#include <stdio.h>
+# include <stdio.h>
 
 typedef struct		s_gnl
 {
-	int 			fd;
-	char 			*content;
+	int				fd;
+	char			*content;
 	struct s_gnl	*next;
 }					t_gnl;
 
